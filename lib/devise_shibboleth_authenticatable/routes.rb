e3e86ad32f07772ca4dcbbc27e332b9b0127bc23
Dan Reedy <danreedy@gmail.com>
2013-04-12 13:31:59 -0500

    Updating routes for Rails version 4

--- lib/devise_shibboleth_authenticatable/routes.rb
+++ lib/devise_shibboleth_authenticatable/routes.rb
@@ -4,7 +4,7 @@ ActionDispatch::Routing::Mapper.class_eval do
   def devise_shibboleth_authenticatable(mapping, controllers)
     resource :session, :only => [], :controller => controllers[:shibboleth_sessions], :path => "" do
       get :new, :path => mapping.path_names[:sign_in], :as => "new"
-      match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy"
+      match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy", via: mapping.sign_out_via 
     end
   end
 end
